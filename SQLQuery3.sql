753ac9111600eb8297947e8d49e1b4d794fd82a6
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-07 22:37:00 +0530

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,20 @@
+
+
+
+
+
+drop table products
+
+create table products
+(
+product_id int identity primary key,
+product_name varchar(255) not null,
+unit_price dec(10,2) check(unit_price>0),
+discounted_price dec(10,2)check(discounted_price>0),
+check(discounted_price<unit_price)
+);
+insert into products values('bikes',125,20)
+insert into products values('bikes',null,null)
+insert into products values('mouse',20,220)
+
+select * from products
