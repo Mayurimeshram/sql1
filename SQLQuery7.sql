ff5535083c3253119148d2022e9ccfb33962a5ef
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-07 22:43:35 +0530

    Add files via upload

---
+++ SQLQuery7.sql
@@ -0,0 +1,14 @@
+
+
+insert into products3 values('mouse',0)
+insert into products3 values('mouse',0)
+select * from products3
+
+drop table products3
+CREATE TABLE products3(
+    product_id INT IDENTITY PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    unit_price DEC(10,2) NOT NULL
+);
+alter table products3
+add constraint positive_price check (unit_price>0);
